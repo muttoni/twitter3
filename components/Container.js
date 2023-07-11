06cc7504ec56a8e7032fe5a725b8a936b4d13f18
Justin <cjustinobi@gmail.com>
2023-07-11 05:29:03 +0100

    Remove manual tweet update

--- components/Container.js
+++ components/Container.js
@@ -99,13 +99,7 @@ export default function Container() {
         </form>
       </div>
       <hr />
-      <h3>Your Tweets
-      <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-arrow-clockwise" viewBox="0 0 16 16">
-        <path fillRule="evenodd" d="M8 3a5 5 0 1 0 4.546 2.914.5.5 0 0 1 .908-.417A6 6 0 1 1 8 2v1z"/>
-        <path d="M8 4.466V.534a.25.25 0 0 1 .41-.192l2.36 1.966c.12.1.12.284 0 .384L8.41 4.658A.25.25 0 0 1 8 4.466z"/>
-      </svg>
-      </button>
-      </h3>
+      <h3>Your Tweets</h3>
         {
         tweetList.length > 0 ?
           tweetList.map((tweet) => {
