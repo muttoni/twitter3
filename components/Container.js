c51fcfcfc871e21277273f6849fda47e1ccc3212
Justin <cjustinobi@gmail.com>
2023-07-11 05:19:26 +0100

    Remove manual tweet update and clear errors that shows on the console

--- components/Container.js
+++ components/Container.js
@@ -35,7 +35,16 @@ export default function Container() {
         }
       })
     }
-  }, [lastTransactionId])
+
+    const fetchTweets = async () => {
+      await getTweets(user?.addr);
+    };
+
+    if (user?.addr) {
+      fetchTweets();
+    }
+    
+  }, [lastTransactionId, user])
 
   const getTweets = async (account) => {
     account = user?.addr;
@@ -78,7 +87,7 @@ export default function Container() {
     <div className={styles.container}>
       <div>
         <form onSubmit={createTweet}>
-        <label for="tweet">Create a new Tweet</label>
+        <label htmlFor="tweet">Create a new Tweet</label>
         <textarea 
           id="tweetContents"               
           placeholder="I feel..."
@@ -91,9 +100,8 @@ export default function Container() {
       </div>
       <hr />
       <h3>Your Tweets
-      <button onClick={getTweets} className={styles.refresh}>
-      <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-clockwise" viewBox="0 0 16 16">
-        <path fill-rule="evenodd" d="M8 3a5 5 0 1 0 4.546 2.914.5.5 0 0 1 .908-.417A6 6 0 1 1 8 2v1z"/>
+      <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-arrow-clockwise" viewBox="0 0 16 16">
+        <path fillRule="evenodd" d="M8 3a5 5 0 1 0 4.546 2.914.5.5 0 0 1 .908-.417A6 6 0 1 1 8 2v1z"/>
         <path d="M8 4.466V.534a.25.25 0 0 1 .41-.192l2.36 1.966c.12.1.12.284 0 .384L8.41 4.658A.25.25 0 0 1 8 4.466z"/>
       </svg>
       </button>
